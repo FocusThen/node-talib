9e23ad7b0f3f106ea92ac595ce1838d00792f807
Mücahit Tezcan <mtezcan@MagiClick.com>
2023-06-25 21:46:59 +0300

    feat: api done

--- main.js
+++ main.js
@@ -7,18 +7,16 @@ const app = express()
 const port = 3000
 
 app.get('/', async (req, res) => {
-  console.log('getting data')
-
   const symbol = 'ADX'
 
-  yf.historical(
-    {
-      symbol: symbol,
-      from: '2020-01-01',
-      to: '2020-08-01',
-    },
-    (err, data) => {
-      const yfData = data.reduce(
+  try {
+    const yfData = await yf
+      .historical({
+        symbol: symbol,
+        from: '2020-01-01',
+        to: '2020-08-01',
+      })
+      ?.reduce(
         (current, next) => {
           current.open.push(next.open)
           current.high.push(next.high)
@@ -36,28 +34,31 @@ app.get('/', async (req, res) => {
         }
       )
 
-      talib.execute(
-        {
-          name: symbol,
-          startIdx: 0,
-          endIdx: yfData.close.length - 1,
-          open: yfData.open,
-          high: yfData.high,
-          low: yfData.low,
-          close: yfData.close,
-          volume: yfData.volume,
-          optInTimePeriod: 9,
-        },
-        (err, result) => {
-          console.log(err, result)
-          return res.json({
-            yahoo: yfData,
-            talib: result,
-          })
-        }
-      )
+    try {
+      const talibData = await talib.execute({
+        name: symbol,
+        startIdx: 0,
+        endIdx: yfData.close.length - 1,
+        open: yfData.open,
+        high: yfData.high,
+        low: yfData.low,
+        close: yfData.close,
+        volume: yfData.volume,
+        optInTimePeriod: 9,
+      })
+
+      await res.json({
+        yahoo: yfData,
+        talib: talibData,
+      })
+    } catch (reson) {
+      res.statusCode = 400
+      res.send(reson)
     }
-  )
+  } catch (reson) {
+    res.statusCode = 400
+    res.send(reson)
+  }
 })
 
 app.listen(port, () => {
